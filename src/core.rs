fe1a3325665c685e3941cfee1d38a166078c16e2
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-09-07 20:15:31 +0500

    Add a function to print a list of file names of available licenses

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,17 @@
 use std::error::Error;
+use std::fs;
+
+pub fn print_licences_templates_list() {
+    let licenses_templates = fs::read_dir("licenses/templates").unwrap();
+    for license_template in licenses_templates {
+        println!(
+            "{}",
+            license_template.unwrap().file_name().to_str().unwrap()
+        )
+    }
+}
 
 pub fn run() -> Result<(), Box<dyn Error>> {
+    print_licences_templates_list();
     Ok(())
 }
