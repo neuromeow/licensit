faf47fe3d3bee0147db325e9b8217438cec417e7
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-09-08 20:05:17 +0500

    Refactor function to print a list of names of available licenses

--- src/core.rs
+++ src/core.rs
@@ -1,43 +1,23 @@
-use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 
-#[allow(dead_code)]
-pub fn print_licence_template_files_list() {
-    let licence_template_files = fs::read_dir("licenses/templates").unwrap();
-    for licence_template_file in licence_template_files {
-        println!(
-            "{}",
-            licence_template_file.unwrap().file_name().to_str().unwrap()
-        )
-    }
-}
+const LICENSES_NAMES: [(&str, &str); 7] = [
+    ("agpl-3.0", "GNU Affero General Public License v3.0"),
+    ("apache-2.0", "Apache License 2.0"),
+    ("gpl-3.0", "GNU General Public License v3.0"),
+    ("lgpl-3.0", "GNU Lesser General Public License v3.0"),
+    ("mit", "MIT License"),
+    ("mpl-2.0", "Mozilla Public License 2.0"),
+    ("unlicense", "The Unlicense"),
+];
 
-pub fn print_licences_list() {
-    let licenses_names = HashMap::from([
-        ("agpl-3.0", "GNU Affero General Public License v3.0"),
-        ("apache-2.0", "Apache License 2.0"),
-        ("gpl-3.0", "GNU General Public License v3.0"),
-        ("lgpl-3.0", "GNU Lesser General Public License v3.0"),
-        ("mit", "MIT License"),
-        ("mpl-2.0", "Mozilla Public License 2.0"),
-        ("unlicense", "The Unlicense"),
-    ]);
-    let license_template_files = fs::read_dir("licenses/templates").unwrap();
-    for license_template_file in license_template_files {
-        let license_template_file_basename = license_template_file.unwrap().file_name();
-        let license_full_name = licenses_names
-            .get(license_template_file_basename.to_str().unwrap())
-            .unwrap();
-        println!(
-            "{: <15} {}",
-            license_template_file_basename.to_str().unwrap(),
-            license_full_name
-        );
+fn print_licence_names_list() {
+    for (license_abbreviation, license_name) in LICENSES_NAMES {
+        println!("{license_abbreviation: <15} {license_name}");
     }
 }
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    print_licences_list();
+    print_licence_names_list();
     Ok(())
 }
