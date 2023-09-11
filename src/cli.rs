06567a4c7795d0fe90f305e90a284f3f1548750d
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-09-11 19:40:14 +0500

    Add a simple unit test to verify command line interface

--- src/cli.rs
+++ src/cli.rs
@@ -13,3 +13,14 @@ pub enum Commands {
     /// Print a list of available open source licenses
     List,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_cli() {
+        use clap::CommandFactory;
+        Cli::command().debug_assert()
+    }
+}
