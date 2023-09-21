d553c942fe6589f470ce11853c982cd5179df94d
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-09-21 19:50:23 +0500

    Add a simple unit test for the render license function

--- src/util.rs
+++ src/util.rs
@@ -127,4 +127,94 @@ SOFTWARE.
             mit_license_template_expected_content
         );
     }
+
+    #[test]
+    fn test_render_license() {
+        let default_license_author = "default_license_author";
+        let default_license_year = 2023 as u16;
+        let another_license_author = "another_license_author";
+        let another_license_year = 2024 as u16;
+        let mit_license_template = fetch_license_template("mit");
+        let lgpl_3_0_license_template = fetch_license_template("lgpl-3.0");
+        let mit_license_default_fillers_expected_content = "MIT License
+
+Copyright (c) 2023 default_license_author
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the \"Software\"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+";
+        let mit_license_another_fillers_expected_content = "MIT License
+
+Copyright (c) 2024 another_license_author
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the \"Software\"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+";
+        assert_eq!(
+            render_licence(
+                "lgpl-3.0",
+                lgpl_3_0_license_template,
+                default_license_author,
+                &default_license_year
+            ),
+            lgpl_3_0_license_template
+        );
+        assert_eq!(
+            render_licence(
+                "lgpl-3.0",
+                lgpl_3_0_license_template,
+                another_license_author,
+                &another_license_year
+            ),
+            lgpl_3_0_license_template
+        );
+        assert_eq!(
+            render_licence(
+                "mit",
+                mit_license_template,
+                default_license_author,
+                &default_license_year
+            ),
+            mit_license_default_fillers_expected_content
+        );
+        assert_eq!(
+            render_licence(
+                "mit",
+                mit_license_template,
+                another_license_author,
+                &another_license_year
+            ),
+            mit_license_another_fillers_expected_content
+        );
+    }
 }
