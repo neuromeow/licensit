efb1fffaf9924f254fe1037d88f10ee2b3c97b2c
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-12-08 19:20:02 +0500

    Remove unused methods for fetching and rendering license

--- src/license_renderers.rs
+++ src/license_renderers.rs
@@ -75,28 +75,4 @@ impl LicenseDescriptions {
         licences_list.pop();
         licences_list
     }
-
-    pub fn fetch_license_template(&self, license_abbreviation: &str) -> &str {
-        let license_template = "";
-        for license_description in &self.licenses {
-            if license_abbreviation == license_description.abbreviation {
-                return license_description.fetch_license_template();
-            }
-        }
-        license_template
-    }
-
-    pub fn render_licence(
-        &self,
-        license_abbreviation: &str,
-        license_author: &str,
-        license_year: &u32,
-    ) -> String {
-        for license_description in &self.licenses {
-            if license_abbreviation == license_description.abbreviation {
-                return license_description.render_licence(license_author, license_year);
-            }
-        }
-        String::new()
-    }
 }
